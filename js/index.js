b37d6737b5968fb7bc3404cbef96e00edec2ddc4
CloudSVC <93499895+CloudSVC@users.noreply.github.com>
2022-10-17 17:58:24 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@
 const pc_app = document.getElementById("app");
 const head = document.getElementById("head");
 const nav = document.getElementById("nav");
+const mobile_head = document.getElementById("mobile_head");
+const tables = document.getElementById("tables");
 nav.style.display="none";
 app.style.display="none";
 head.style.display="none";
+mobile_head.style.display="none";
+tables.style.transform= "translateX("+ "-250px" +")";
 
 // 	document.getElementById("body").style.backgroundColor="rgba(146,182,213)";
 window.onload=function(){
@@ -21,6 +25,7 @@ window.onload=function(){
 		app.style.display="";
 		head.style.display="";
 		nav.style.display="";
+		mobile_head.style.display="";
 	},10)
 }
 window.onscroll = function() {
@@ -31,4 +36,14 @@ window.onscroll = function() {
 	}else {
 		document.getElementById('nav').style.transform = "translateY("+ "-90px" +")";
 	}
+}
+function turn_on() {
+	tables.style.transform = "translateX("+ "0px" +")";
+	document.getElementById("switch").getAttributeNode("onclick").value="turn_off()";
+	document.getElementById("switch").style.backgroundImage="url('./images/tablesuioff.png')";
+}
+function turn_off(){
+	tables.style.transform = "translateX("+ "-250px" +")";
+	document.getElementById("switch").getAttributeNode("onclick").value="turn_on()";
+	document.getElementById("switch").style.backgroundImage="url('./images/tablesui.png')";
 }
